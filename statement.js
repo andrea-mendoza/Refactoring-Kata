8a05ad0ee6a2ef7d462e7e0628531622a0512c1c
andymehi <andymehi@hotmail.com>
2019-03-06 00:24:45 -0400

    change amountFor names

--- statement.js
+++ statement.js
@@ -1,18 +1,18 @@
-function amountFor(play, perf) {
+function amountFor(play, aPerfonmance) {
   let result = 0;
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerfonmance.audience > 30) {
+        result += 1000 * (aPerfonmance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerfonmance.audience > 20) {
+        result += 10000 + 500 * (aPerfonmance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerfonmance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
