9f0b971c9f77ba58497aa3e08066b30a4f25e2a1
andymehi <andymehi@hotmail.com>
2019-03-06 12:36:12 -0400

    totalVolumeCredits

--- statement.js
+++ statement.js
@@ -1,15 +1,13 @@
 function statement (invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
+
   for (let perf of invoice.performances) {
-
-    volumeCredits += volumeCreditsFor(perf);
-
-    // print line for this order
+  // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
-  }
+    totalAmount += amountFor(perf); }
+
+  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
@@ -55,6 +53,13 @@ function statement (invoice, plays) {
           style: "currency", currency: "USD",
           minimumFractionDigits: 2
         }).format(aNumber/100);
+  }
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 }
 
