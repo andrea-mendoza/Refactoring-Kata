ad053d7f6d8c28cfca587e8f1ccd74ad11fcb2ff
andymehi <andymehi@hotmail.com>
2019-03-06 12:43:25 -0400

    rename appleSause

--- statement.js
+++ statement.js
@@ -1,17 +1,12 @@
 function statement (invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-  // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf); }
+  }
 
-  let volumeCredits = totalVolumeCredits();
-
-  result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`; return result;
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -61,6 +56,12 @@ function statement (invoice, plays) {
     }
     return volumeCredits;
   }
+
+  function totalAmount() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf); }
+    return totalAmount; }
 }
 
 export default statement;
