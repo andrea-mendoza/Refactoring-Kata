d2528ef04de00b137c641da3096f278a817d6e0a
andymehi <andymehi@hotmail.com>
2019-03-06 22:37:26 -0400

    Deleting invoice parameter

--- statement.js
+++ statement.js
@@ -1,10 +1,13 @@
 function statement (invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
-function renderPlainText (invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+function renderPlainText (data, plays) {
+  let result = `Statement for ${data.customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
@@ -15,14 +18,14 @@ function renderPlainText (invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf); }
     return result;
   }
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
