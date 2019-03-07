72e3b281a894938b7fa0c1aaaeb46566057ad1a9
andymehi <andymehi@hotmail.com>
2019-03-06 22:56:57 -0400

    moving totalVolumeCredits

--- statement.js
+++ statement.js
@@ -4,6 +4,8 @@ function statement (invoice, plays) {
   statementData.performances = invoice.performances;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -56,6 +58,14 @@ function statement (invoice, plays) {
     }
     return result;
   }
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
+
 }
 function renderPlainText (data, plays) {
   let result = `Statement for ${data.customer}\n`;
@@ -65,17 +75,9 @@ function renderPlainText (data, plays) {
   }
 
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
 
   return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
